47a5e1422d0816cc413c06a76b0b29cd45ac977d
YuShanCh <m820618@gmail.com>
2022-05-03 00:07:51 +0800

    商品功能- 實作修改商品

--- springboot-mall/src/main/java/com/Yushan/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/Yushan/springbootmall/service/ProductService.java
@@ -2,10 +2,13 @@ package com.Yushan.springbootmall.service;
 
 import com.Yushan.springbootmall.dto.ProductRequest;
 import com.Yushan.springbootmall.model.Product;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
