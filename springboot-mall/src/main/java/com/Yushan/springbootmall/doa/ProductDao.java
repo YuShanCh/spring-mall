d8615e07e2afd238da5024292c16b69e4ad1b8cd
YuShanCh <m820618@gmail.com>
2022-05-02 21:49:23 +0800

    商品功能- 實作新增商品

--- springboot-mall/src/main/java/com/Yushan/springbootmall/doa/ProductDao.java
+++ springboot-mall/src/main/java/com/Yushan/springbootmall/doa/ProductDao.java
@@ -1,9 +1,11 @@
 package com.Yushan.springbootmall.doa;
 
+import com.Yushan.springbootmall.dto.ProductRequest;
 import com.Yushan.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/Yushan/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/Yushan/springbootmall/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.Yushan.springbootmall.service;
 
+import com.Yushan.springbootmall.dto.ProductRequest;
 import com.Yushan.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
